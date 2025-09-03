0feb0922eff4522d457ef8a432ec780c1f1d8c14
Helena Pedro <mbeua94@gmail.com>
2025-09-02 22:41:06 -0500

    Problem4, solved

--- CS390-2025/src/W1L2_HW_Asgmt/Prob4.java
+++ CS390-2025/src/W1L2_HW_Asgmt/Prob4.java
@@ -5,33 +5,46 @@ import java.util.Scanner;
 public class Prob4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String repeat;
 
-        System.out.print("Digit the number of terms(N): ");
-        int num = sc.nextInt();
+        do {
+            System.out.print("Digit the number of terms (N): ");
+            int num = sc.nextInt();
 
-        System.out.print("Which one you want to calculate? (a/b): ");
-        String choice = sc.next();
+            System.out.print("Which one you want to calculate? (a/b): ");
+            String choice = sc.next();
 
-        double sum = 0.0;
+            double sum = switch (choice.toLowerCase()) {
+                case "a" -> {
+                    double x = 0.0;
+                    // Série (a): 1 - 1/3 + 1/5 - 1/7 + ...
+                    for (int i = 1; i <= num; i++) {
+                        double term = Math.pow(-1, i + 1) / (2.0 * i - 1);
+                        x += term;
+                    }
+                    yield x;
+                }
+                case "b" -> {
+                    double y = 0.0;
+                    // Série (b): 1/2 - 2/4 + 3/8 - 4/16 + ...
 
-        if (choice.equals("a") || choice.equals("A")) {
-            // Series (a): 1 - 1/3 + 1/5 - 1/7 + ...
-            for (int i = 1; i <= num; i++) {
-                double term = (Math.pow(-1, i + 1)) / (2.0 * i - 1);
-                sum += term;
-            }
-            System.out.printf("Series (a), sum of %d terms = %.3f%n", num, sum);
-        } else if (choice.equals("b") || choice.equals("B")) {
-            // Series (b): 1/2 - 2/4 + 3/8 - 4/16 + ...
-            for (int i = 1; i <= num; i++) {
-                double term = Math.pow(-1, i + 1) * i / Math.pow(2, i);
-                sum += term;
-            }
-            System.out.printf("Series (b), sum of %d terms = %.3f%n", num, sum);
-        } else {
-            System.out.println("Invalid choice. Please choose 'a' or 'b'.");
-        }
+                    for (int i = 1; i <= num; i++) {
+                        double term = Math.pow(-1, i + 1) * i / Math.pow(2, i);
+                        y += term;
+                    }
+                    yield y;
+                }
+                default -> 0.0;
+            };
+
+            System.out.printf("Sum = %.3f%n", sum);
+
+            System.out.print("Do you want to calculate another series? (y/n): ");
+            repeat = sc.next();
+
+        } while (repeat.equalsIgnoreCase("y"));
 
         sc.close();
+        System.out.println("Goodbye and thank you!");
     }
 }
