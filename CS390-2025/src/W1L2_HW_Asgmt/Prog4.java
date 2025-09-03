f2494438898d4f3c3fd4551900a73ab2fb66e49a
Helena Pedro <mbeua94@gmail.com>
2025-09-02 23:21:12 -0500

    Updated Prog4 logic based on todays class

--- CS390-2025/src/W1L2_HW_Asgmt/Prog4.java
+++ CS390-2025/src/W1L2_HW_Asgmt/Prog4.java
@@ -1,31 +1,30 @@
 package W1L2_HW_Asgmt;
 
+import java.util.Arrays;
+
 public class Prog4 {
     public static void main(String[] args) {
-        String[] strArray = {"horse", "dog", "cat", "horse", "dog"};
-        String[] uniqueStrArray = new String[strArray.length];
-        int uniqueCount = 0;
+        String[] input = {"horse", "dog", "cat", "horse", "dog"};
+        String[] unique = new String[input.length];
+        int collected = 0;
 
-        for (int i = 0; i < strArray.length; i++) {
-            String currentStr = strArray[i];
-            boolean isDuplicated = false;
+        for (String str : input) {
+            boolean seen = false;
 
-            for (int j = 0; j < uniqueCount; j++) {
-                if (uniqueStrArray[j].equals(currentStr)) {
-                    isDuplicated = true;
+            for (int i = 0; i < collected; i++) {
+                if (unique[i].equals(str)) {
+                    seen = true;
                     break;
                 }
             }
 
-            if (!isDuplicated) {
-                uniqueStrArray[uniqueCount] = currentStr;
-                uniqueCount++;
+            if (!seen) {
+                unique[collected++] = str;
             }
         }
 
-        // This is to print the unique values in the new array
-        for (int i = 0; i < uniqueCount; i++)  {
-            System.out.println(uniqueStrArray[i]);
-        }
+        String[] res = new String[collected];
+        System.arraycopy(unique, 0, res, 0, collected);
+        System.out.println(Arrays.toString(res));
     }
 }
