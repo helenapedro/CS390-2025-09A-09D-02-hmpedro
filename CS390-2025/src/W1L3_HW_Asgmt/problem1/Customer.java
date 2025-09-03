fdae89be86f95be805f0a9a4260a187e5dfe378d
Helena Pedro <mbeua94@gmail.com>
2025-09-03 14:47:17 -0500

    Problem 1 solved

---
+++ CS390-2025/src/W1L3_HW_Asgmt/problem1/Customer.java
@@ -0,0 +1,52 @@
+package W1L3_HW_Asgmt.problem1;
+
+public class Customer {
+    private final String firstName;
+    private final String lastName;
+    private final String socSecurityNum;
+    private Address shippingAddress;
+    private Address billingAddress;
+
+    public Customer(String firstName, String lastName, String socSecurityNum) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socSecurityNum = socSecurityNum;
+    }
+
+    @Override
+    public String toString() {
+        return "[" +
+                " firstName = '" + firstName + '\'' +
+                ", lastName = '" + lastName + '\'' +
+                ", socSecurityNum = '" + socSecurityNum +
+                ']';
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSocSecurityNum() {
+        return socSecurityNum;
+    }
+
+    public Address getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(Address shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public Address getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(Address billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+}
---
+++ CS390-2025/src/W1L3_HW_Asgmt/problem1/Prob1Test.java
@@ -0,0 +1,40 @@
+package W1L3_HW_Asgmt.problem1;
+
+public class Prob1Test {
+    public static void main(String[] args) {
+        // Instances of Customer
+        Customer c1 = new Customer("Jane", "Doe", "332-221-4444");
+        Customer c2 = new Customer("Joe", "Smith", "987-654-3848");
+        Customer c3 = new Customer("Janeth", "Smith", "345-987-6996");
+
+        // Address
+        Address a1 = new Address("1000 N Webber ST", "Fairfield", "IA", "52556");
+        Address a2 = new Address("233 S Wacker Dr", "Chicago", "IL", "60606");
+        Address a3 = new Address("111 S Michigan Ave", "Chicago", "IL", "60603");
+        Address a4 = new Address("456 Oak Ave", "Skokie", "IL", "60077");
+        Address a5 = new Address("789 Pine Rd", "Buffalo Grove", "IL", "60089");
+        Address a6 = new Address("123 Main St", "Evanston", "IL", "60201");
+
+        c1.setBillingAddress(a1);
+        c1.setShippingAddress(a4);
+        c2.setBillingAddress(a2);
+        c2.setShippingAddress(a5);
+        c3.setBillingAddress(a3);
+        c3.setShippingAddress(a6);
+
+        // Array of Customer
+        Customer[] customers = new Customer[3];
+        customers[0] = c1;
+        customers[1] = c2;
+        customers[2] = c3;
+
+        System.out.println("\nCustomers whose billingAddress is located in the city of Chicago:");
+        for (Customer c : customers) {
+            Address address = c.getBillingAddress();
+            if (address.getCity().equalsIgnoreCase("Chicago")) {
+                System.out.println(c);
+            }
+        }
+
+    }
+}
