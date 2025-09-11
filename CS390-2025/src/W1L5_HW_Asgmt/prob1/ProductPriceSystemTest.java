50170a636a1b6ab58880bae6bd78de742b7581af
Helena Pedro <91161075+helenapedro@users.noreply.github.com>
2025-09-10 19:46:05 -0500

    Update ProductPriceSystemTest.java

--- CS390-2025/src/W1L5_HW_Asgmt/prob1/ProductPriceSystemTest.java
+++ CS390-2025/src/W1L5_HW_Asgmt/prob1/ProductPriceSystemTest.java
@@ -5,6 +5,8 @@ import W1L5_HW_Asgmt.prob1.productpricingsystem.Electronics;
 import W1L5_HW_Asgmt.prob1.productpricingsystem.Furniture;
 import W1L5_HW_Asgmt.prob1.productpricingsystem.Product;
 
+import java.util.Arrays;
+
 public class ProductPriceSystemTest {
     /* Follow LSP & Dynamic Binding:
         - to configure specific behavior/state of an object - create subclass directly
