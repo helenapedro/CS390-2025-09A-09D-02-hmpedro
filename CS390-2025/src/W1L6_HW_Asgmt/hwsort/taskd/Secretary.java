4ee225ab8be437ee94bae48a57941bc325a400a3
Helena Pedro <mbeua94@gmail.com>
2025-09-11 21:29:13 -0500

    Lesson 6 hw updated

--- CS390-2025/src/W1L6_HW_Asgmt/hwsort/taskd/Secretary.java
+++ CS390-2025/src/W1L6_HW_Asgmt/hwsort/taskd/Secretary.java
@@ -5,21 +5,25 @@ import java.time.LocalDate;
 public class Secretary extends DeptEmployee {
 	private double netSalary;
 	private double overtimeHours;
-	public Secretary(String name, double salary, LocalDate hireDate, double overtimeHours){
-		super(name,salary,hireDate);
+
+	public Secretary(String name, double salary, LocalDate hireDate, double overtimeHours) {
+		super(name, salary, hireDate);
 		this.overtimeHours = overtimeHours;
 	}
+
 	public double getOvertimeHours() {
 		return overtimeHours;
 	}
-	
-	//overrides superclass method
+
+	// overrides superclass method
+	@Override
 	public double getSalary() {
-		netSalary = super.getSalary() + 12*overtimeHours;
+		netSalary = super.getSalary() + 12 * overtimeHours;
 		return netSalary;
 	}
+
 	@Override
 	public String toString() {
-		return "name=" + super.getName() + ", salary=" + netSalary + ", hireDate=" + super.getHireDate();
+		return "name=" + super.getName() + ", salary=" + getSalary() + ", hireDate=" + super.getHireDate();
 	}
 }
